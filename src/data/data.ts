066ea4739b87c4e6cdc2e04d090acc975cae95f1
Tatiroa994 <tatico994@gmail.com>
2021-07-19 16:39:20 -0500

    add data-var CSS-accests

---
+++ src/data/data.ts
@@ -0,0 +1,70 @@
+export const profile = {
+  aboutme:
+    'Desarrolladora web, enfocada en el  área frontend, apasionada por el diseño creativo de aplicaciones, que generen  un ambiente positivo y amigable con el usuario; Me encanta estar en un mundo cambiante, que constantemente me reta a transformarme y adaptarme a estos cambios, por ello elegi el camino tecnologico, desde aqui puedo aprender cada día y aplicar la experiencia que he adquirido en los ultimos años, en entornos laborales que me permitieron trabajar en equipo, resolver  problemas, aportar ideas innovadoras y asi fortalecer la confianza en lo que hago.',
+  skills: [
+    {
+      skill: 'Angular',
+      urlImg: '../../assets/angular-icon.png',
+    },
+    {
+      skill: 'TypeScrip',
+      urlImg: '../../assets/ts-icon.png',
+    },
+    {
+      skill: 'JavaScript',
+      urlImg: '../../assets/js-icon.png',
+    },
+    {
+      skill: 'CSS',
+      urlImg: '../../assets/css-icon.png',
+    },
+    {
+      skill: 'HTML',
+      urlImg: '../../assets/HTML5-icon.png',
+    },
+    {
+      skill: 'Web Component',
+      urlImg: '../../assets/wc-icon.png',
+    },
+    {
+      skill: 'Firebase',
+      urlImg: '../../assets/firebase-icon.png',
+    },
+  ],
+  phone: '3193760856',
+  gitHub: 'https://github.com/tatiroa994',
+  linkedin: 'https://www.linkedin.com/in/tatiroa994/',
+  email: 'tatico994@gmail.com',
+  cv: 'pendiente',
+};
+
+export const proyects = [
+  {
+    proyect: 'Burguer Queen',
+    skill: ['Angular', 'TypeScript', 'CSS'],
+    'repo-link': 'https://github.com/tatiroa994/BOG002-burger-queen',
+    'demo-link': 'https://burguer-queen-77d39.web.app/home',
+    img: '../../assets/bq-icon.png',
+  },
+  {
+    proyect: 'EcoIdeate',
+    skill: ['JavaScript', 'HTML', 'CSS', 'Web Component'],
+    'repo-link': 'https://github.com/tatiroa994/BOG002-social-network',
+    'demo-link': 'https://tatiroa994.github.io/BOG002-social-network/',
+    img: '../../assets/sn-icon.png',
+  },
+  {
+    proyect: 'Md-links',
+    skill: ['JavaScript', 'NodeJS'],
+    'repo-link': 'https://github.com/tatiroa994/BOG002-md-links',
+    'demo-link': 'https://github.com/tatiroa994/BOG002-md-links/packages/809641',
+    img: '../../assets/mdLinks-icon.png',
+  },
+  {
+    proyect: 'Secretos',
+    skill: ['JavaScript', 'HTML', 'CSS'],
+    'repo-link': 'https://github.com/tatiroa994/BOG002-md-links',
+    'demo-link': 'https://github.com/tatiroa994/BOG002-md-links/packages/809641',
+    img: '../../assets/secrets-icon.png',
+  },
+];
