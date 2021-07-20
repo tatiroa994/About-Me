e1f579cf2a381acaf7d9a2d23fea3e7a7ac23b71
Tatiroa994 <tatico994@gmail.com>
2021-07-19 20:10:40 -0500

    estructura

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { HomeComponent } from './app/home/home.component';
-import { AboutMeComponent } from './app/about-me/about-me.component';
-import { AllProyectsComponent } from './app/all-proyects/all-proyects.component';
-import { ContacMeComponent } from './app/contac-me/contac-me.component';
-import { ProyectComponent } from './app/proyect/proyect.component';
-import { NavegationBarComponent } from './app/navegation-bar/navegation-bar.component';
+
+import { NavegationBarComponent } from './navegation-bar/navegation-bar.component';
 import { RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { AllProyectsComponent } from './all-proyects/all-proyects.component';
+import { ContacMeComponent } from './contac-me/contac-me.component';
+import { ProyectComponent } from './proyect/proyect.component';
 
 @NgModule({
   declarations: [
